3b56916cb01c779060da4bfd1ad1a64a59f2862b
vitalets <noginsk@rambler.ru>
2025-09-12 19:54:04 +0200

    link to fixed header pr

--- test/helpers/fixed-header.js
+++ test/helpers/fixed-header.js
@@ -1,5 +1,6 @@
 import { Header } from "selenium-webdriver/bidi/networkTypes.js";
 
+// See: https://github.com/SeleniumHQ/selenium/pull/16321
 Header.prototype.asMap = function () {
   const map = new Map();
   map.set("name", this.name);
