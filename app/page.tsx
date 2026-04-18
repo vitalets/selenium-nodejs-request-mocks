9c1234e426d0961d8d1772b32ac831d970b99994
vitalets <noginsk@rambler.ru>
2026-04-18 16:02:46 +0200

    update types

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,13 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <h1>Hello, SeleniumConf 2025!</h1>
+      <h1>Hello, SeleniumConf 2026!</h1>
       <ul>
         <li>
-          <Link href="/client-side-api-call">
-            Page with client-side API call
-          </Link>
+          <Link href="/client-side-api-call">Page with client-side API call</Link>
         </li>
         <li>
-          <Link href="/server-side-api-call">
-            Page with server-side API call
-          </Link>
+          <Link href="/server-side-api-call">Page with server-side API call</Link>
         </li>
       </ul>
     </>
