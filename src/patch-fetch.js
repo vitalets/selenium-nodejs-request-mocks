b2ab19f0bfa81e7d04522070930bd16de802b36c
vitalets <noginsk@rambler.ru>
2026-04-19 15:46:45 +0200

    update rmp and move to src/app

---
+++ src/patch-fetch.js
@@ -0,0 +1,6 @@
+import { setupFetchInterceptor } from 'request-mocking-protocol/fetch';
+
+setupFetchInterceptor(async () => {
+  const { headers } = await import('next/headers.js');
+  return headers();
+});
