b7f876114e2a66da327eb9e7f0ab6da2ca8fb24c
vitalets <1473072+vitalets@users.noreply.github.com>
2026-05-07 21:21:12 +0200

    prettier

--- test/client-mocks.test.js
+++ test/client-mocks.test.js
@@ -23,10 +23,14 @@ afterEach(async () => {
 });
 
 it('non-empty list (client mocks)', async () => {
-  await mockClientSideRequest(network, 'https://jsonplaceholder.typicode.com/users?_limit=6', [
-    { id: 1, name: 'User 1' },
-    { id: 2, name: 'User 2' },
-  ]);
+  await mockClientSideRequest(
+    network,
+    'https://jsonplaceholder.typicode.com/users?_limit=6',
+    [
+      { id: 1, name: 'User 1' },
+      { id: 2, name: 'User 2' },
+    ],
+  );
 
   await driver.get('http://localhost:3000');
   const users = await driver.wait(until.elementsLocated(By.css('.users li')), 3000);
@@ -36,7 +40,11 @@ it('non-empty list (client mocks)', async () => {
 });
 
 it('empty list (client mocks)', async () => {
-  await mockClientSideRequest(network, 'https://jsonplaceholder.typicode.com/users?_limit=6', []);
+  await mockClientSideRequest(
+    network,
+    'https://jsonplaceholder.typicode.com/users?_limit=6',
+    [],
+  );
 
   await driver.get('http://localhost:3000');
   const content = await driver.wait(until.elementLocated(By.css('.empty')), 3000);
@@ -45,7 +53,11 @@ it('empty list (client mocks)', async () => {
 });
 
 it('error (client mocks)', async () => {
-  await mockClientSideRequest(network, 'https://jsonplaceholder.typicode.com/users?_limit=6', 500);
+  await mockClientSideRequest(
+    network,
+    'https://jsonplaceholder.typicode.com/users?_limit=6',
+    500,
+  );
 
   await driver.get('http://localhost:3000');
   const content = await driver.wait(until.elementLocated(By.css('.error')), 3000);
